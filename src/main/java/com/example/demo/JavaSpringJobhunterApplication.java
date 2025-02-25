61b9e09377301c47c9918392a726313b30b2ce6a
lephuduy298 <leduyphucat@gmail.com>
2025-02-25 14:01:00 +0700

    update video 16

---
+++ src/main/java/com/example/demo/JavaSpringJobhunterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringJobhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringJobhunterApplication.class, args);
+	}
+
+}
