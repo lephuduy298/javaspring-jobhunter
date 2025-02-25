a84735475671303249e0d5114924b23fec32acb2
lephuduy298 <leduyphucat@gmail.com>
2025-02-25 15:09:32 +0700

    update chaper 3

--- src/main/java/com/example/demo/JavaSpringJobhunterApplication.java
+++ src/main/java/com/example/demo/JavaSpringJobhunterApplication.java
@@ -3,7 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
+
 public class JavaSpringJobhunterApplication {
 
 	public static void main(String[] args) {
