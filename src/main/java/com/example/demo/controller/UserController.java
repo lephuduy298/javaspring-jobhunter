e1c39a62664e70d104f94c3a6057a15e87c5c126
lephuduy298 <leduyphucat@gmail.com>
2025-02-26 23:48:20 +0700

    update chaper 5

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,8 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,16 +27,37 @@ public class UserController {
         this.userService = userService;
     }
 
-    // @GetMapping("/user/create")
-    @PostMapping("/user/create")
-    public User createUser(@RequestBody User user) {
-        // User user = new User();
-        // user.setName("Le Phu Duy");
-        // user.setEmail("leduyphucat@gmail.com");
-        // user.setPassword("lephuduy");
+    // @GetMapping("/users/create")
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
 
         User newUser = this.userService.handleSaveUser(user);
 
-        return newUser;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
+        this.userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body("delete user");
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchUserByID(id));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userService.fetchAllUser();
+        return ResponseEntity.ok(users);
+        // return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser(@RequestBody User newUser) {
+        User currentUser = this.userService.handleUpdateUser(newUser);
+
+        return ResponseEntity.status(HttpStatus.OK).body(currentUser);
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
@@ -16,6 +19,34 @@ public class UserService {
 
     public User handleSaveUser(User user) {
         return this.userRepository.save(user);
+    }
+
+    public void deleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public User fetchUserByID(long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        }
+        return null;
+    }
+
+    public List<User> fetchAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User newUser) {
+        User currentUser = this.fetchUserByID(newUser.getId());
+        if (newUser != null) {
+            currentUser.setEmail(newUser.getEmail());
+            currentUser.setName(newUser.getName());
+            currentUser.setPassword(newUser.getPassword());
+            this.userRepository.save(currentUser);
+        }
+        return currentUser;
+
     }
 
 }
