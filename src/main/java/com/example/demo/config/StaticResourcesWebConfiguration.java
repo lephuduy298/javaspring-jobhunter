d4db3c484ebb7721386c97e642c4fade3bf8d4b4
lephuduy298 <leduyphucat@gmail.com>
2025-03-18 19:18:21 +0700

    update download file video 102

--- src/main/java/com/example/demo/config/StaticResourcesWebConfiguration.java
+++ src/main/java/com/example/demo/config/StaticResourcesWebConfiguration.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class StaticResourcesWebConfiguration implements WebMvcConfigurer {
     @Value("${lephuduy.upload-file.base-uri}")
-    private String baseUri;
+    private String baseURI;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/storage/**")
-                .addResourceLocations(baseUri);
+                .addResourceLocations(baseURI);
     }
 }
--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
@@ -7,7 +8,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +32,7 @@ public class FileController {
     private final AuthController authController;
 
     @Value("${lephuduy.upload-file.base-uri}")
-    private String baseUri;
+    private String baseURI;
 
     private final FileService fileService;
 
@@ -39,7 +45,7 @@ public class FileController {
     @ApiMessage("Upload single file")
     public ResponseEntity<ResUploadFileDTO> upload(
             @RequestParam("folder") String folder,
-            @RequestParam(value = "file", required = false) MultipartFile file)
+            @RequestParam(name = "file", required = false) MultipartFile file)
             throws URISyntaxException, IOException, StorageException {
 
         // validate
@@ -59,13 +65,40 @@ public class FileController {
         }
 
         // create folder
-        this.fileService.createDirectory(baseUri + folder);
+        this.fileService.createDirectory(baseURI + folder);
 
         // save file
         String currentFile = this.fileService.store(file, folder);
 
         ResUploadFileDTO res = new ResUploadFileDTO(currentFile, Instant.now());
         return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping("/files")
+    @ApiMessage("download a file")
+    public ResponseEntity<Resource> hanldeDownloadFile(
+            @RequestParam(name = "folder", required = false) String folder,
+            @RequestParam(name = "fileName", required = false) String fileName)
+            throws StorageException, URISyntaxException, FileNotFoundException {
+
+        if (fileName == null || folder == null) {
+            throw new StorageException("Missing required params : (fileName or folder) in query params.");
+        }
+
+        // check file exist (and not a directory)
+        long fileLength = this.fileService.getFileLength(fileName, folder);
+        if (fileLength == 0) {
+            throw new StorageException("File with name = " + fileName + " not found.");
+        }
+
+        // download a file
+        InputStreamResource resource = this.fileService.getResource(fileName, folder);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
 }
